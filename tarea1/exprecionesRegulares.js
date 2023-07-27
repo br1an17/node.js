ee72ca4a4c4858575ea95b98f6e9cdb845394dc1
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-07-26 23:23:22 -0300

    1 ejercicio

---
+++ tarea1/exprecionesRegulares.js
@@ -0,0 +1,19 @@
+// Clase de operadores y expresiones
+
+// contraseña simple numeros y letras
+// contraseña media numeros, letras y al menos una mayuscula
+// contraseña alta numeros, letras, al menos una mayuscula y caracter especial
+
+
+
+
+const passwordSimple = /^(?=.*[A-Za-z])(?=.*\d).{6,}$/
+
+const passwordMedia =/A/
+const passwordAlta =/a/
+
+
+
+
+
+ console.log(passwordSimple.test("hole12323"))
