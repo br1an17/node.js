e9d2611874319c42512b594b148816c9add83c9f
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-08-01 20:37:33 -0300

    volvimos

--- tarea1/exprecionesRegulares.js
+++ tarea1/exprecionesRegulares.js
@@ -10,7 +10,8 @@
 const passwordSimple = /^(?=.*[A-Za-z])(?=.*\d).{6,}$/
 
 const passwordMedia =/^(?=.*[A-Za-z])(?=.*[A-Z])(?=.*\d).{6,}$/
-const passwordAlta =/a/
+const passwordAlta =/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:'",.<>?[\]\\/]).{8,}$/
+
 
 
 
@@ -18,3 +19,4 @@ const passwordAlta =/a/
 
  console.log(passwordSimple.test("hole12323"))
  console.log(passwordMedia.test("hOle12323"))
+ console.log(passwordAlta.test("hOle12323/"))
