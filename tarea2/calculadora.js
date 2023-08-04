2a695032a6dcc70df449cd94be79917092876dca
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-08-03 22:57:13 -0300

    tarea2

---
+++ tarea2/calculadora.js
@@ -0,0 +1,15 @@
+// Crear un archivo calculadora.js y soporte 1 parámetro de texto
+// El programa debe distinguir que operación aritmética quiere hacer el usuario y responder
+// Como parte de la consigna es delimitar el alcance de la calculadora
+// Importante: La calculadora no debe dar errores en ninguna circunstancia, si hay alguna operación que no soporte debe decirlo
+
+// node calculadora.js "me gustaría sumar 5 y 6"
+// node calculadora.js "me sumas 5 con 6"
+// node calculadora.js "tengo que hacer una suma de 5 y 6, cuanto me da?"
+// node calculadora.js "me dividis 9 con 3"
+
+
+const calculadora = ( num)=>{
+
+
+}
