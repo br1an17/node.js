9526dc4d7005a0664d8eb0491cf4827b52352199
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-08-04 23:45:31 -0300

    calculadora

--- tarea2/calculadora.js
+++ tarea2/calculadora.js
@@ -9,7 +9,49 @@
 // node calculadora.js "me dividis 9 con 3"
 
 
-const calculadora = ( num)=>{
+// Importamos el módulo 'readline' para leer el parámetro de texto
+const readline = require('readline');
 
+// Creamos la interfaz para leer la entrada
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+// Función para realizar la operación matemática
+function calcularOperacion(texto) {
+  const palabras = texto.split(' ');
+
+  // Buscamos los números en el texto
+  const numeros = palabras.filter(palabra => !isNaN(Number(palabra)));
+
+  // Verificamos el tipo de operación en el texto
+  if (texto.includes('sumar') || texto.includes('mas')) {
+    const resultado = numeros.reduce((total, num) => total + Number(num), 0);
+    console.log(`Resultado: ${resultado}`);
+  } else if (texto.includes('restar') || texto.includes('menos')) {
+    const resultado = numeros.reduce((total, num) => total - Number(num));
+    console.log(`Resultado: ${resultado}`);
+  } else if (texto.includes('multiplicar') || texto.includes('por')) {
+    const resultado = numeros.reduce((total, num) => total * Number(num), 1);
+    console.log(`Resultado: ${resultado}`);
+  } else if (texto.includes('dividir') || texto.includes('entre')) {
+    const resultado = numeros.reduce((total, num) => total / Number(num));
+    console.log(`Resultado: ${resultado}`);
+  } else {
+    console.log('Operación no soportada. Intente nuevamente.');
+  }
+
+  rl.close();
+}
+
+// Leemos el parámetro de texto pasado al ejecutar el script
+const parametroTexto = process.argv[2];
+
+// Verificamos si se proporcionó un parámetro de texto
+if (parametroTexto) {
+  calcularOperacion(parametroTexto);
+} else {
+  console.log('Debe proporcionar un parámetro de texto con la operación a realizar.');
+  rl.close();
 }
