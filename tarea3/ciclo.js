0dd0b456dc57025d8976c6e974a6a9c926487733
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-08-05 21:47:37 -0300

    tarea 3

---
+++ tarea3/ciclo.js
@@ -0,0 +1,16 @@
+// En el rango de 0 a 10000 la idea es que la máquina encuentre un número ingresado por el usuario desde la consola en la menor cantidad de intentos posibles
+// tarea para entregar en clase 9
+// let numero = //se ingresa por consola
+// let contador = 0;
+
+
+
+let num = ""
+
+let contador = 0
+
+const algo = () => {
+
+
+
+};
